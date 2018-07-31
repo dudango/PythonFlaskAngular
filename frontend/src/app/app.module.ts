6d6a81ae430e97251a1cbccaab77ba32033a8fbb
dad <ragnorok1@gmail.com>
2018-07-31 11:16:03 -0400

    Adding Long_Description support to the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,7 @@ import * as Auth0 from 'auth0-web';
 import {CallbackComponent} from './callback.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {
-  MatToolbarModule, MatButtonModule, MatCardModule
+  MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule
 } from '@angular/material';
 
 
@@ -39,6 +39,7 @@ const appRoutes: Routes = [
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
+    MatInputModule,
   ],
 
   providers: [ExamsApiService],
